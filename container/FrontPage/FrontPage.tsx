c8beccb240365b65204004d2e015466ce22ca3c1
jungwoo <youngsun.lee1@gmail.com>
2021-12-09 00:29:17 +0900

    remove view component

--- container/FrontPage/FrontPage.tsx
+++ container/FrontPage/FrontPage.tsx
@@ -2,36 +2,23 @@ import { SyntheticEvent, useContext } from 'react';
 import { ModalContext } from '../../components/utils/modal/modalContext';
 
 import useInput from '../../hooks/useInput';
-import FrontPageView from './FrontPage.view';
+import styles from './FrontPage.module.css';
 
 function FrontPage() {
   const { handleModal } = useContext(ModalContext);
 
   //region Inputs
-  const {
-    value: enteredName,
-    isValid: nameIsValid,
-    hasError: nameInputHasError,
-    valueChangeHandler: nameChangeHandler,
-    inputBlurHandler: nameBlurHandler,
-    reset: nameReset,
-  } = useInput((value: string) => value.trim() !== '');
 
-  const {
-    value: passwordValue,
-    isValid: passwordIsValid,
-    hasError: passwordInputHasError,
-    valueChangeHandler: passwordChangeHandler,
-    inputBlurHandler: passwordBlurHandler,
-    reset: passwordReset,
-  } = useInput((value: string) => value.trim() !== '');
+  const nameInput = useInput((value: string) => value.trim() !== '');
+  const passwordInput = useInput((value: string) => value.trim() !== '');
+
   //endregion
 
   //region Handlers
   const handleSubmit = (event: SyntheticEvent) => {
     event.preventDefault();
 
-    if (!nameIsValid || !passwordIsValid) {
+    if (!nameInput.isValid || !passwordInput.isValid) {
       handleModal('input is not valid, enter something to inputbox');
       return;
     }
@@ -40,29 +27,44 @@ function FrontPage() {
   };
   //endregion
 
-  //region Props To View
-  const props = {
-    nameInput: {
-      value: enteredName,
-      isValid: nameIsValid,
-      hasError: nameInputHasError,
-      valueChangeHandler: nameChangeHandler,
-      inputBlurHandler: nameBlurHandler,
-      reset: nameReset,
-    },
-    passwordInput: {
-      value: passwordValue,
-      isValid: passwordIsValid,
-      hasError: passwordInputHasError,
-      valueChangeHandler: passwordChangeHandler,
-      inputBlurHandler: passwordBlurHandler,
-      reset: passwordReset,
-    },
-    handleSubmit,
-  };
-  //endregion
-
-  return (<FrontPageView {...props} />);
+  return (
+    <div className={styles.FrontContainer}>
+      <div className={styles.FrontContent}>
+        <p className={styles.FrontContext}>
+          THIS IS FRONT PAGE
+        </p>
+        <p className={styles.FrontContext}>
+          TODO 1 : Add Backend (nextjs)
+        </p>
+        <p className={styles.FrontContext}>
+          TODO 2 : Add DB (mongoDB or mysql)
+        </p>
+        <p className={styles.FrontContext}>
+          TODO 3 : Add Chat (socket.io)
+        </p>
+        <p className={styles.FrontContext}>
+          TODO 4 : Add Auth(sign in/out)
+        </p>
+        <div>
+          <input
+            type="text"
+            value={nameInput.value}
+            onChange={nameInput.valueChangeHandler}
+            onBlur={nameInput.inputBlurHandler}
+          />
+        </div>
+        <div>
+          <input
+            type="text"
+            value={passwordInput.value}
+            onChange={passwordInput.valueChangeHandler}
+            onBlur={passwordInput.inputBlurHandler}
+          />
+        </div>
+        <button type="submit" className={styles.FrontBtn} onClick={handleSubmit}>SUBMIT</button>
+      </div>
+    </div>
+  );
 }
 
 export default FrontPage;
